e19dc9c464ffc9dd07a8d1c5c003728b7991ac1e
Vladyslav Havrylenko <vladyslav.havrylenko@3ss.tv>
2020-03-12 08:59:35 +0200

    Init

---
+++ app/src/main/java/com/evaluation/retrofit/MainActivity.java
@@ -0,0 +1,46 @@
+package com.evaluation.retrofit;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.evaluation.dagger.data.DataComponent;
+import com.evaluation.fragment.BaseFragment;
+import com.evaluation.navigation.Navigator;
+
+import javax.inject.Inject;
+
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Inject
+    Navigator mNavigator;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        DataComponent.Injector.init();
+
+        setContentView(R.layout.activity_main);
+
+        DataComponent.Injector.getComponent().inject(this);
+
+        ButterKnife.bind(this);
+
+        mNavigator.init(this);
+        mNavigator.showMainFragment();
+    }
+
+    @Override
+    public void onBackPressed() {
+        BaseFragment currentFragment = mNavigator.getContentFragment();
+
+        // Dispatch back event to the current fragment.
+        if (currentFragment != null && currentFragment.onBackPressed()) {
+            return;
+        }
+        super.onBackPressed();
+    }
+}
